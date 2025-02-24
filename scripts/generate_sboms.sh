cb2ff584edbd9bd3c00262932dadce243ba85aba
Pierre-Louis Lacorte <pierrelouis.lacorte@datadoghq.com>
2025-02-24 10:57:18 +0000

    feat: add to description

--- scripts/generate_sboms.sh
+++ scripts/generate_sboms.sh
@@ -2,11 +2,15 @@
 
 set -e  # Exit on error
 
+echo "### Tools Used for SBOM Generation:" >> $GITHUB_STEP_SUMMARY
+
 # Define SBOM output directory
 SBOM_DIR="/tmp/sboms"
 mkdir -p "$SBOM_DIR"
 
+
 # Generate SBOM with DataDog osv-scanner
+echo "- datadog-sbom-generator" >> $GITHUB_STEP_SUMMARY # adding to the overall action summary
 echo "Scanning SBOM with datadog-sbom-generator..."
 $GITHUB_WORKSPACE/osv-scanner/osv-scanner --skip-git --recursive --experimental-only-packages --no-ignore --format=cyclonedx-1-5 --output="${SBOM_DIR}/datadog-sbom-generator.json" .
 
