fea9251a79a771abeeaadd6138614185d5ee7162
Pierre-Louis Lacorte <pierrelouis.lacorte@datadoghq.com>
2025-02-24 17:14:19 +0000

    clean current actions

--- scripts/upload_sboms.sh
+++ scripts/upload_sboms.sh
@@ -2,6 +2,8 @@
 
 set -e  # Exit on error
 
+cd $TARGET_DIR
+
 echo "Uploading SBOMs..."
 echo "### Generated SBOM and associated SHA:" >> $GITHUB_STEP_SUMMARY
 
@@ -10,24 +12,35 @@ for file in "$SBOM_DIR"/*; do
     if [[ -f "$file" ]]; then
         echo "Uploading SBOM: $file..."
 
-        # Generate unique SHAs even if the same repo + commit is compared twice, that is irrelevant here
+        # Generate unique SHAs even if the same repo + commit is compared twice.
+        # Because we will upload one SBOM per generation tool on the same commit,
+        # we force a commit SHA update, letting us upload several SBOMs. 
         LAST_COMMITTER_NAME=$(git log -1 --pretty=format:'%an')
         LAST_COMMITTER_EMAIL=$(git log -1 --pretty=format:'%ae')
         git config --global user.email "$LAST_COMMITTER_EMAIL"
         git config --global user.name "$LAST_COMMITTER_NAME"
         git commit --amend --no-edit
 
+        # Add description to the summary of the SHA generated for a specific generation tool 
         FILENAME=$(basename "$file" .json)
-        COMMIT_SHA="${GITHUB_SHA}"
+        COMMIT_SHA=$(git rev-parse HEAD)
         echo "- **$FILENAME**: $COMMIT_SHA" >> $GITHUB_STEP_SUMMARY
 
-        # export GIT_DIR="$TARGET_DIR/.git"
-        echo "Current directory: $(pwd)"
-        git config --local --list
-
+        # Unfortunately, when executing a Github action, some git env variables cannot be overriten
+        # no matter the current context of execution.
+        # Assuming 2 git repositories (outer-repo and inner-repo) as follow:
+        # outer-repo/
+        # ├── .git/
+        # ├── README.md
+        # └── inner-repo/
+        #     ├── .git/
+        #     ├── README.md
+        #     └── some-file.txt
+        # Executing `datadog-ci sbom upload` at `outer-repo/` or `outer-repo/inner-repo/` would report the same result git context.
+        # A workaround is to manually set DD specific env variables, that way, datadog-ci reports correct git information.
         export DD_GIT_REPOSITORY_URL=$(git config --get remote.origin.url)
         export DD_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-        export DD_GIT_COMMIT_SHA=$(git rev-parse HEAD)
+        export DD_GIT_COMMIT_SHA=COMMIT_SHA
         export DD_GIT_COMMIT_MESSAGE=$(git log -1 --pretty=format:'%s')
         export DD_GIT_COMMIT_COMMITTER_DATE=$(git log -1 --pretty=format:'%cI')
         export DD_GIT_COMMIT_COMMITTER_EMAIL=$(git log -1 --pretty=format:'%ce')
@@ -35,18 +48,6 @@ for file in "$SBOM_DIR"/*; do
         export DD_GIT_COMMIT_AUTHOR_DATE=$(git log -1 --pretty=format:'%aI')
         export DD_GIT_COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty=format:'%ae')
         export DD_GIT_COMMIT_AUTHOR_NAME=$(git log -1 --pretty=format:'%an')
-
-        echo "DD_GIT_REPOSITORY_URL: $DD_GIT_REPOSITORY_URL"
-        echo "DD_GIT_BRANCH: $DD_GIT_BRANCH"
-        echo "DD_GIT_COMMIT_SHA: $DD_GIT_COMMIT_SHA"
-        echo "DD_GIT_TAG: $DD_GIT_TAG"
-        echo "DD_GIT_COMMIT_MESSAGE: $DD_GIT_COMMIT_MESSAGE"
-        echo "DD_GIT_COMMIT_COMMITTER_DATE: $DD_GIT_COMMIT_COMMITTER_DATE"
-        echo "DD_GIT_COMMIT_COMMITTER_EMAIL: $DD_GIT_COMMIT_COMMITTER_EMAIL"
-        echo "DD_GIT_COMMIT_COMMITTER_NAME: $DD_GIT_COMMIT_COMMITTER_NAME"
-        echo "DD_GIT_COMMIT_AUTHOR_DATE: $DD_GIT_COMMIT_AUTHOR_DATE"
-        echo "DD_GIT_COMMIT_AUTHOR_EMAIL: $DD_GIT_COMMIT_AUTHOR_EMAIL"
-        echo "DD_GIT_COMMIT_AUTHOR_NAME: $DD_GIT_COMMIT_AUTHOR_NAME"
 
         # Upload SBOM file using datadog-ci
         datadog-ci sbom upload "$file"
