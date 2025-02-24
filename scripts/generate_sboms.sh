0dae3df935c4f8d86b56c1b54671420f3f1f642e
Pierre-Louis Lacorte <pierrelouis.lacorte@datadoghq.com>
2025-02-24 12:00:32 +0000

    fix: log current repository and add sha to job summary

--- scripts/generate_sboms.sh
+++ scripts/generate_sboms.sh
@@ -4,14 +4,11 @@ set -e  # Exit on error
 
 echo "Generating SBOMs..."
 
-echo "### Tools Used for SBOM Generation:" >> $GITHUB_STEP_SUMMARY
-
 # Define SBOM output directory
 mkdir -p "$SBOM_DIR"
 
 # Generate SBOM with DataDog osv-scanner
 echo "Scanning SBOM with datadog-sbom-generator..."
-echo "- datadog-sbom-generator" >> $GITHUB_STEP_SUMMARY # adding to the overall action summary
 $GITHUB_WORKSPACE/osv-scanner/osv-scanner --skip-git --recursive --experimental-only-packages --no-ignore --format=cyclonedx-1-5 --output="${SBOM_DIR}/datadog-sbom-generator.json" .
 
 echo "SBOMs generation and vulnerability scan complete."
--- scripts/upload_sboms.sh
+++ scripts/upload_sboms.sh
@@ -3,10 +3,13 @@
 set -e  # Exit on error
 
 echo "Uploading SBOMs..."
+echo "### Generated SBOM and associated SHA:" >> $GITHUB_STEP_SUMMARY
 
 # Iterate over all SBOM files in /tmp/sboms
 for file in "$SBOM_DIR"/*; do
     if [[ -f "$file" ]]; then
+        echo "Uploading SBOM: $file..."
+
         # Generate unique SHAs even if the same repo + commit is compared twice, that is irrelevant here
         LAST_COMMITTER_NAME=$(git log -1 --pretty=format:'%an')
         LAST_COMMITTER_EMAIL=$(git log -1 --pretty=format:'%ae')
@@ -14,12 +17,19 @@ for file in "$SBOM_DIR"/*; do
         git config --global user.name "$LAST_COMMITTER_NAME"
         git commit --amend --no-edit
 
-        echo "Uploading SBOM: $file"
+        FILENAME=$(basename "$file" .json)
+        COMMIT_SHA="${GITHUB_SHA}"
+        echo "- **$FILENAME**: $COMMIT_SHA" >> $GITHUB_STEP_SUMMARY
+
+        CURRENT_REPO=$(git remote get-url origin)
+        echo "Current repository: $CURRENT_REPO"
+        CURRENT_SHA=$(git rev-parse HEAD)
+        echo "Current commit SHA: $CURRENT_SHA"
 
         # Upload SBOM file using datadog-ci
         datadog-ci sbom upload "$file"
         
-        echo "Successfully uploaded: $file"
+        echo "Successfully uploaded: $file."
     fi
 done
 
